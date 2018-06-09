be9a6dcf511007d7ff7b68f03f53518ebb170446
BiancaArtola <bian.artola@hotmail.com>
2018-06-09 20:14:40 -0300

    .

--- app/Http/Controllers/controladorEliminar.php
+++ app/Http/Controllers/controladorEliminar.php
@@ -2,20 +2,17 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 class controladorEliminar extends Controller
 {
     public function eliminar(Request $req){
-    	$request = Request::create('/api/unRecorrido/{req->nombre_url}', 'get');
+    	var_dump("hola");
+    	$request = Request::create('/api/unRecorrido/{req->_id}', 'get');
 		$response = Route::dispatch($request);
-		$response= $response->getOriginalContent();
-		$data = json_decode($response);	
-		var_dump($data);
-
-		//hay q elimianr aca pero me da miedo hacerlo por si se elimina cualquiera
+		
+		$response -> delete();
 
 		return redirect('/');
     }
