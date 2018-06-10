9427d6305506432eb55ed1c08ddf2abc2bb69d46
BiancaArtola <bian.artola@hotmail.com>
2018-06-10 19:25:19 -0300

    modificacion 2

--- resources/views/nuevoReco.blade.php
+++ resources/views/nuevoReco.blade.php
@@ -15,7 +15,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light" id="navbar_1"><a class="navbar-brand" href="http://ciudadesturisticas-admin.herokuapp.com/"><img src="https://png.icons8.com/metro/1600/worldwide-location.png" width="30" height="30" alt=""/></a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><a class="navbar-brand" href="https://ciudadesturisticas.herokuapp.com/"><b>CIUDADES TURISTICAS</b></a>
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><a class="navbar-brand" href="http://ciudadesturisticas-admin.herokuapp.com/"><b>CIUDADES TURISTICAS</b></a>
         <div
             class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto"></ul>
