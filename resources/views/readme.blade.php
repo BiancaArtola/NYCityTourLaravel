ed41b33e7d8cbb530ce108025da5bf0b46e73e9c
BiancaArtola <bian.artola@hotmail.com>
2018-06-14 13:02:42 -0300

    Agregado placeid al readme

--- resources/views/readme.blade.php
+++ resources/views/readme.blade.php
@@ -8,5 +8,11 @@
 Para ingresar: 
 -Usuario: hola@hola.com
 -Contraseña: 123456
+
+Places id de prueba:
+- ChIJ8-JRXoxZwokRGPiQ9Ek0L84
+- ChIJDcEvbPVZwokRETEfzXATns8
+- ChIJUxPQH4BNwokRaQur1y-PJ90
+- ChIJfcsCZi3XwokRqLqitSH-JYQ
 </body>
 </html>
